fc42eb54aa046b063a2fa3583d9267a13d77d136
Seán Labastille <git@lsr.li>
2017-11-15 16:02:56 +0100

    Add logo index

--- Sources/App/Controllers/LogoController.swift
+++ Sources/App/Controllers/LogoController.swift
@@ -6,12 +6,15 @@
 //
 
 import Vapor
+import Foundation
 import HTTP
 
 final class LogoController: ResourceRepresentable, EmptyInitializable {
 
     func index(_ req: Request) throws -> ResponseRepresentable {
-        throw Abort(.notImplemented)
+        var logos = [JSON]()
+        (1...6).map({logos.append(try! Logo(thumbnailURL: try! req.uri.appendingPathComponent("storage/icon_1024x1024-\($0).png").makeFoundationURL(), logoURL: try! req.uri.appendingPathComponent("storage/icon_120x120-\($0).png").makeFoundationURL(), publishedAt: Date(), id: UUID()).makeJSON())})
+        return JSON.array(logos)
     }
 
     func show(_ requ: Request, logo: Logo) throws -> ResponseRepresentable {
